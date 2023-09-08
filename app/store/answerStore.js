8e82a4fca056b62658a2513d86d6d652083ad06f
manuimfeld <manu_jadir@hotmail.com>
2023-09-08 17:59:00 -0300

    refactor code

---
+++ app/store/answerStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+export const useAnswerStore = create((set) => ({
+  answerData: {
+    totalQuestions: 0,
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+    currentQuestion: null,
+  },
+  setCurrentQuestion: (data) =>
+    set((state) => ({
+      answerData: {
+        ...state.answerData,
+        currentQuestion: data,
+      },
+    })),
+
+  incrementCorrectAnswer: () =>
+    set((state) => ({
+      answerData: {
+        ...state.answerData,
+        correctAnswers: state.answerData.correctAnswers + 1,
+        totalQuestions: state.answerData.totalQuestions + 1,
+      },
+    })),
+
+  incrementIncorrectAnswer: () =>
+    set((state) => ({
+      answerData: {
+        ...state.answerData,
+        incorrectAnswers: state.answerData.incorrectAnswers + 1,
+        totalQuestions: state.answerData.totalQuestions + 1,
+      },
+    })),
+}));
