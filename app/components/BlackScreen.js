19d0933540ab2d923b34ea89aebbf57cb90796c5
manuimfeld <manu_jadir@hotmail.com>
2023-09-08 00:00:18 -0300

    fix class conditional background

--- app/components/BlackScreen.js
+++ app/components/BlackScreen.js
@@ -1,3 +1,15 @@
+import { useEffect } from "react";
+import { playingStore } from "../store/playingStore";
+
 export const BlackScreen = () => {
-  return <div className={`absolute z-2 top-0 w-full h-full bg-black`}></div>;
+  const playing = playingStore((state) => state.playing);
+  console.log(playing);
+
+  return (
+    <div
+      className={`absolute z-2 top-0 w-full h-full ${
+        playing === true ? "bg-black" : "bg-none"
+      }`}
+    ></div>
+  );
 };
