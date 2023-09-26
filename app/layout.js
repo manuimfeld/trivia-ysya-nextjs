504922a21bc6980e42d91fdbb7afcd4ca382a839
manuimfeld <manu_jadir@hotmail.com>
2023-09-26 10:57:22 -0300

    Refactor RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Providers from "./Providers";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -15,27 +16,30 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <Providers>
-          <main
-            className="min-h-screen w-screen"
-            role="main"
-            aria-label="Contenedor de la aplicación"
-          >
-            <Image
-              src="/bg-antezana.jpg"
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              alt="Portada del disco Antezana 247 de YSY A"
-              className="z-[-1] fixed w-full h-full"
-            />
-            {children}
-          </main>
-        </Providers>
-      </body>
-    </html>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+      </Head>
+      <Providers>
+        <main
+          className={`${poppins.className} min-h-screen w-screen`}
+          role="main"
+          aria-label="Contenedor de la aplicación"
+        >
+          <Image
+            src="/bg-antezana.jpg"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            alt="Portada del disco Antezana 247 de YSY A"
+            className="z-[-1] fixed w-full h-full"
+          />
+          {children}
+        </main>
+      </Providers>
+    </>
   );
 }
-//
