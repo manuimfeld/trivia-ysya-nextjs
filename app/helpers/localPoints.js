b1ee0a850f69640f92425593034a32b3c62a65be
manuimfeld <manu_jadir@hotmail.com>
2023-10-03 12:35:09 -0300

    Remove innecesary if

--- app/helpers/localPoints.js
+++ app/helpers/localPoints.js
@@ -3,29 +3,25 @@ import { useAnswerStore } from "../store/answerStore";
 export const savePoints = async () => {
   const { correctAnswers } = useAnswerStore.getState().answerData;
   let localPoints = localStorage.getItem("points");
-  if (typeof window !== "undefined") {
-    if (localPoints !== null) {
-      localPoints = parseInt(localPoints);
-      localStorage.setItem("points", (localPoints += correctAnswers));
-    } else {
-      localStorage.setItem("points", parseInt(correctAnswers));
-    }
+
+  if (localPoints !== null) {
+    localPoints = parseInt(localPoints);
+    localStorage.setItem("points", (localPoints += correctAnswers));
+  } else {
+    localStorage.setItem("points", parseInt(correctAnswers));
   }
 };
 
 export const deletePoints = async () => {
-  if (typeof window !== "undefined") {
-    localStorage.removeItem("points");
-  }
+  localStorage.removeItem("points");
 };
 
 export const getLocalPoints = () => {
   let localPoints = localStorage.getItem("points");
-  if (typeof window !== "undefined") {
-    if (localPoints !== null) {
-      return parseInt(localPoints);
-    } else {
-      return null;
-    }
+
+  if (localPoints !== null) {
+    return parseInt(localPoints);
+  } else {
+    return null;
   }
 };
